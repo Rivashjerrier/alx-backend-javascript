90ca46dd6ef3bb7dc71f6eed50652fbdc3e588a4
Rivashjerrier <rivijerrier@gmail.com>
2023-03-28 17:03:48 -0700

    Clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,8 +1,10 @@
 export default function cleanSet(set, startString) {
   const values = [];
-  for (const value of set) {
-    if (value.startsWith(startString)) {
-      values.push(value.substring(startString.length));
+  if (startString && typeof startString === 'string') {
+    for (const value of set) {
+      if (value.startsWith(startString) && value !== startString) {
+        values.push(value.substr(startString.length));
+      }
     }
   }
   return values.join('-');
