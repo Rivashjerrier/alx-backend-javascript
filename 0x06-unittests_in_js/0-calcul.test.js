933078a78a28859f0962df785f3d06ef25d1221b
Rivashjerrier <rivijerrier@gmail.com>
2023-08-23 21:11:57 +0000

    Update 0-calcul.test.js

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -2,7 +2,7 @@ const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', function () {
-    it('should return the sum of rounded numbers', function () {
+    it('should return the sum of two numbers rounded off', function () {
         assert.strictEqual(calculateNumber(1, 3), 4);
         assert.strictEqual(calculateNumber(1, 3.7), 5);
         assert.strictEqual(calculateNumber(1.2, 3.7), 5);
